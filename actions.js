92e19726b806c4bf0aab1f53b35d66642f2e0934
rk4github <rk4ravikumar@gmail.com>
2016-12-05 20:37:30 +0530

    updated actions.js
    updated actions.js to make it work with latest cassandra-driver.

--- actions.js
+++ actions.js
@@ -1,42 +1,42 @@
 module.exports = {
-	InsertContact : function (req, res, pool){
-		pool.connect(function(err, keyspace){
+	InsertContact : function (req, res, client){
+		client.connect(function(err, keyspace){
 		    if(err){
 		    	throw(err);
 		    } else {
 		    	var post = req.body;
-				pool.cql("INSERT INTO contact (email, name, phone, address, city) VALUES (?,?,?,?,?)", [post.email, post.name, post.phone, post.address, post.city], function(err, results){
+				client.execute("INSERT INTO contact (email, name, phone, address, city) VALUES (?,?,?,?,?)", [post.email, post.name, post.phone, post.address, post.city], function(err, results){
 					res.redirect('/');
 				});
 		    }
 		});
 	},
-	UpdateContact : function (req, res, pool){
-		pool.connect(function(err, keyspace){
+	UpdateContact : function (req, res, client){
+		client.connect(function(err, keyspace){
 		    if(err){
 		    	throw(err);
 		    } else {
 		    	var post = req.body;
-				pool.cql("UPDATE contact SET name = ?, phone = ?, address = ?, city = ? WHERE email = ?", [post.name, post.phone, post.address, post.city, post.contactEdit], function(err, results){
+				client.execute("UPDATE contact SET name = ?, phone = ?, address = ?, city = ? WHERE email = ?", [post.name, post.phone, post.address, post.city, post.contactEdit], function(err, results){
 					res.redirect('/');
 				});
 		    }
 		});
 	},
-	DeleteContact : function (req, res, pool){
-		pool.connect(function(err, keyspace){
+	DeleteContact : function (req, res, client){
+		client.connect(function(err, keyspace){
 		    if(err){
 		    	throw(err);
 		    } else {
 		    	var post = req.body;
-				pool.cql("DELETE FROM contact WHERE email = ?", [post.contactDel], function(err, results){
+				client.execute("DELETE FROM contact WHERE email = ?", [post.contactDel], function(err, results){
 					res.redirect('/');
 				});
 		    }
 		});
 	},
-	LoadContacts : function (req, res, pool){
-		pool.connect(function(err, keyspace){
+	LoadContacts : function (req, res, client){
+		client.connect(function(err, keyspace){
 		    if(err){
 		    	throw(err);
 		    } else {
@@ -45,14 +45,13 @@ module.exports = {
 		    	if(typeof(req.query.email) != "undefined"){
 		    		query += " WHERE email = '"+req.query.email+"'";
 		    	}
-				pool.cql(query, [], function(err, results){
+				client.execute(query, [], function(err, results){
 					var data = [];
-					results.forEach(function(row){
-						var obj = {};
-					  	row.forEach(function(name,value,ts,ttl){
-					    	obj[name] = value;
-					  	});
-					  	data.push(obj);
+					results.rows.forEach(function(row){
+						if( !row ){
+						    var obj = row;					  	
+					  	    data.push(obj);
+						}
 					});
 					res.send(data);
 				});
